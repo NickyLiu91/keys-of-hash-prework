43d998b4326899165b2f88a622fcfb2f55d3f680
Nicky Liu <nickyliu91@gmail.com>
2018-08-22 16:16:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,11 @@
 class Hash
   def keys_of(*arguments)
-    array = [
-
-    ]
+    array = []
+    hash.each do |key, value|
+      if arguments.include value
+        array << key
+      end
+    end
+    array
   end
 end
